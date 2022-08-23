2ba5c37edd3aa2c4a3af92c35a00ca66cedf55c7
VantaBlack <Vantablackzz@yandex.ru>
2022-08-23 09:33:31 +0400

    counter in alert

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
-
+document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener("keydown", function(event) {
     jump();
+    count++;
+    console.log(count)       
 })
+let count = 0;
 function jump() {
     if (dino.classList != "jump") {
         dino.classList.add("jump")
@@ -16,8 +19,10 @@ let isAlive = setInterval (function() {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-        alert("Game Over!");
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 130) {
+        alert(`Game Over! Your score: ${count}`);
+        count = 0;
     }
 }, 10)
-
+});
+  
